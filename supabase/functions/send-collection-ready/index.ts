090dd0d030d54e15ae526e6952f50cf017800420
Digital Gaff <digitalgaff@Digitals-MBP2.lan>
2026-09-03 22:51:44 +0100

    Change SMS sender to alphanumeric ID PitterPotte

--- supabase/functions/send-collection-ready/index.ts
+++ supabase/functions/send-collection-ready/index.ts
@@ -68,7 +68,7 @@ async function sendReadySMS(
   try {
     const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
     const body = new URLSearchParams();
-    body.append('From', fromNumber);
+    body.append('From', 'PitterPotte');
     body.append('To', toNumber);
     body.append('Body', message);
 
